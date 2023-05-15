7399c04fda4afa6bf5df74bb806ffc68803d025e
TBRWEB - Desenvolvimento de Sites <macsueldias@hotmail.com>
2023-05-15 09:11:10 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import '../styles/header.scss'
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div>
+        <img src="/logo.svg" alt="to.do"/>
+      </div>
+    </header>
+  )
+}
